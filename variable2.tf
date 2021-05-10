1a5260416580a340b135a314b7762bf521bfd1e6
hanumantharao19 <63094959+hanumantharao19@users.noreply.github.com>
2021-05-10 21:49:06 +0530

    Create variable2.tf

---
+++ variable2.tf
@@ -0,0 +1,32 @@
+variable "vpcname"{
+
+
+}
+
+variable "subnetname" {
+
+
+}
+
+variable "zone"{
+
+default = "us-central1-c"
+}
+
+variable "region"{
+
+
+}
+variable "instancename" {
+
+
+}
+
+variable "machinetype"   {
+
+}
+
+variable "cidr" {
+
+
+}
